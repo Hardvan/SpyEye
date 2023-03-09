a79a16b74ec12255a0a92eb00126237226f9408c
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-03-09 14:47:15 +0530

    Add Whastapp Message Feature

--- Main/intruder_det.py
+++ Main/intruder_det.py
@@ -3,6 +3,9 @@ import numpy as np
 import time
 from datetime import datetime
 import os
+import mimetypes
+import requests
+import json
 
 """
     ? Extra Features:
@@ -55,8 +58,78 @@ def saveImage(frame, x, y, w, h, time):
 
         # Display the saved face image in a new window
         cv2.imshow(f"Face {current_time}", face)
+
     except:
         pass
+
+    UploadImage(f"./saved/face_{current_time}.jpg", current_time)
+
+
+def UploadImage(path, timer):
+    target = path
+    mime_type = mimetypes.guess_type(target)[0]
+
+    files = {
+        "file": (target, open(target, "rb"), mime_type)
+    }
+
+    headers = {
+        "Authorization": f"Bearer EABU7YlckmlkBAE2S1RepUkns2E3Y64GPk2wOoOZAvom9SjBItNdfOQiBRmBW5tndWlKGdawX4zrPqP4PPclQicKQYwoVV92ZCZANrLErFGZCiDZAX5UYxxDOQJyfV0FaFNUK8bc20xAkdARWDMANyTVB09ZByaoPvZCIbP7UotIXyhWJJMYS35zYa5ZAmq41PSctZAS7GSCZC09exjIwcpNJvB"
+    }
+
+    params = {
+        "messaging_product": "whatsapp",
+        "type": mime_type
+    }
+
+    response = requests.post(
+        f"https://graph.facebook.com/v13.0/113814568315440/media",
+        headers=headers,
+        params=params,
+        files=files
+    )
+
+    result_whatsapp_media = response.json()
+    http_code = response.status_code
+
+    MEDIA_OBJECT_ID = result_whatsapp_media["id"]
+    print(MEDIA_OBJECT_ID)
+    SendMessage(MEDIA_OBJECT_ID, timer)
+
+
+def SendMessage(object_id, timing):
+    url = 'https://graph.facebook.com/v15.0/113814568315440/messages'
+    headers = {
+        'Authorization': 'Bearer EABU7YlckmlkBAE2S1RepUkns2E3Y64GPk2wOoOZAvom9SjBItNdfOQiBRmBW5tndWlKGdawX4zrPqP4PPclQicKQYwoVV92ZCZANrLErFGZCiDZAX5UYxxDOQJyfV0FaFNUK8bc20xAkdARWDMANyTVB09ZByaoPvZCIbP7UotIXyhWJJMYS35zYa5ZAmq41PSctZAS7GSCZC09exjIwcpNJvB',
+        'Content-Type': 'application/json'
+    }
+
+    karan = "7348911401"
+    hardik = "9845072575"
+
+    data = {
+        "messaging_product": "whatsapp",
+        "preview_url": False,
+        "recipient_type": "individual",
+        "to": f"91{karan}",  # Start with 91 for Indian numbers
+        "type": "image",
+        "image": {
+            "id": object_id,
+            "caption": timing
+        }
+
+        # For first time:
+        # "type": "template",
+        # "template": {
+        #     "name": "hello_world",
+        #     "language": {
+        #         "code": "en_US" }
+
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    print(response.content)
 
 
 def drawRectangles(frame, faces):
